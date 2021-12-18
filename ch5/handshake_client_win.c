79e0a942f2d402054322bdbfeacd29cdd1eab585
Edward759 <759263934@qq.com>
2021-12-18 10:35:38 +0800

    add ch5

---
+++ ch5/handshake_client_win.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+
+void ErrorHandling(char* message);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN servAddr;
+
+	char msg1[] = "msg1";
+	char msg2[] = "msg2";
+	char msg3[] = "msg3";
+	char message[30];
+	char* strarr[] = { msg1, msg2, msg3 };
+
+	int idx = 0, readLen = 0, strLen = 0;
+
+	if (argc != 3)
+	{
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup() error!");
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET)
+		ErrorHandling("socket() error");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	servAddr.sin_port = htons(atoi(argv[2]));
+
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ErrorHandling("connect() error!");
+
+	for (int i = 0; i < 3; i++)
+	{
+		recv(hSocket, (char*)(&readLen), 4, 0);
+		recv(hSocket, message, readLen, 0);
+		fputs(message, stdout);
+		fputc('\n', stdout);
+
+		strLen = strlen(strarr[i]) + 1;
+		send(hSocket, (char*)(&strLen), 4, 0);
+		send(hSocket, strarr[i], strLen, 0);
+	}
+
+
+	closesocket(hSocket);
+	WSACleanup();
+	return 0;
+}
+
+void ErrorHandling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
